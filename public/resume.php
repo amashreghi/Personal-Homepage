3537f892b5c57aa8799e51637ee2656351bef26c
amashreghi <ali.mashreghi87@gmail.com>
2017-07-16 01:30:02 -0700

    Fixed resume section formats

--- public/resume.php
+++ public/resume.php
@@ -1,6 +1,21 @@
-<h3 class="resume-section"><span>Education</span></h3>
-
+<h2 class="resume-section" style="margin-top: 30px;"><span class="resume-section-title"><span class="first-title-letters">Edu</span>cation</span></h2>
 <row class="container-fluid">
-
-
+	<div class="col-sm-9">
+		<h4>PhD in Computer Science</h4>
+		<h5>University of Victoria</h5>
+		<p>Thesis: Distributed Algorithms for finding MST with sublinear communication, supervised by Dr. Valerie King</p>
+	</div>
+	<div class="col-sm-3" style="text-align: right;">
+		Victoria, Canada<br> 2016-present
+	</div>
+</row> <!-- row ends -->
+<row class="container-fluid">
+	<div class="col-sm-9">
+		<h4>MSc in Computer Science</h4>
+		<h5>Sharif University of Technology</h5>
+		<p>Thesis: Bi-criteria approximation algorithms for minimum Steiner tree problem, supervised by Dr. Alireza Zarei</p>
+	</div>
+	<div class="col-sm-3" style="text-align: right;">
+		Tehran, Iran<br> 2012-2014
+	</div>
 </row> <!-- row ends -->
