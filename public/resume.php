dc40708ee2a0d125261de3724cbdbc38400af950
amashreghi <ali.mashreghi87@gmail.com>
2017-07-23 00:38:57 -0700

    Resume section complete

--- public/resume.php
+++ public/resume.php
@@ -1,8 +1,8 @@
-<h2 class="resume-section" style="margin-top: 30px;"><span class="resume-section-title"><span class="first-title-letters">Edu</span>cation</span></h2>
+<h2 class="resume-section"><span class="resume-section-title"><span class="first-title-letters">Edu</span>cation</span></h2>
 <row class="container-fluid">
 	<div class="col-sm-9">
 		<h4>PhD in Computer Science</h4>
-		<h5>University of Victoria</h5>
+		<h5>University of Victoria (UVic)</h5>
 		<p>Thesis: Distributed Algorithms for finding MST with sublinear communication, supervised by Dr. Valerie King</p>
 	</div>
 	<div class="col-sm-3" style="text-align: right;">
@@ -12,10 +12,78 @@
 <row class="container-fluid">
 	<div class="col-sm-9">
 		<h4>MSc in Computer Science</h4>
-		<h5>Sharif University of Technology</h5>
+		<h5>Sharif University of Technology (SUT)</h5>
 		<p>Thesis: Bi-criteria approximation algorithms for minimum Steiner tree problem, supervised by Dr. Alireza Zarei</p>
 	</div>
 	<div class="col-sm-3" style="text-align: right;">
 		Tehran, Iran<br> 2012-2014
+	</div>
+</row> <!-- row ends -->
+
+<row class="container-fluid">
+	<div class="col-sm-9">
+		<h4>BSc in Computer Engineering</h4>
+		<h5>Ferdowsi University of Mashhad (FUM)</h5>
+		<p>Project: Implementing RANSAC filter on 3D mesh processing software MeshLab, supervised by Dr. Ahad Harati</p>
+	</div>
+	<div class="col-sm-3" style="text-align: right;">
+		Mashhad, Iran<br> 2008-2012
+	</div>
+</row> <!-- row ends -->
+
+<h2 class="resume-section" ><span class="resume-section-title"><span class="first-title-letters">Pub</span>lications</span></h2>
+<row class="container-fluid">
+	<div class="col-sm-9" >
+		<br>
+		<i>Mashreghi, Ali, and Valerie King.</i> "Time-communication trade-offs for minimum spanning tree construction." Proceedings of the 18th International Conference on Distributed Computing and Networking (ICDCN). ACM.
+	</div>
+	<div class="col-sm-3" style="text-align: right;">
+		2017
+	</div>
+</row> <!-- row ends -->
+<row class="container-fluid">
+	<div class="col-sm-9" >
+		<br>
+		<i>Mashreghi, Ali, and Alireza Zarei </i>. "When Diameter Matters: Parameterized Approximation Algorithms for Bounded Diameter Minimum Steiner Tree Problem." Theory of Computing Systems 58.2: 287-303.
+	</div>
+	<div class="col-sm-3" style="text-align: right;">
+		2016
+	</div>
+</row> <!-- row ends -->
+<row class="container-fluid">
+	<div class="col-sm-9" >
+		<br>
+		<i>Mashreghi, Ali, and Hadi Sadoghi Yazdi</i>. "A recursive algorithm for optimizing differentiation." Journal of Computational and Applied Mathematics 263: 1-13.
+	</div>
+	<div class="col-sm-3" style="text-align: right;">
+		2014
+	</div>
+</row> <!-- row ends -->
+
+<h2 class="resume-section" ><span class="resume-section-title"><span class="first-title-letters">Tea</span>ching Assistantship</span></h2>
+<row class="container-fluid">
+<div class="col-sm-12">
+		During the past few years I have been TAing for the following courses:
+		<ul>
+			<li>Competitive Programming</li>
+			<li>Algorithms and Data Structures</li>
+			<li>Theory of Language and Automata</li>
+		</ul>
+</div>
+</row>
+
+<h2 class="resume-section" ><span class="resume-section-title"><span class="first-title-letters">Ext</span>racurricular Activity</span></h2>
+
+<row class="container-fluid">
+	<div class="col-sm-12" >
+	<b style="font-size:6">Stack Overflow:</b> Mostly answering to algorithms, complexity and graph theory questions. <br>
+
+	<b style="font-size:6">Programming Club:</b> Holding tutorial sessions on algorithms and programming techniques. <br>
+
+	<b style="font-size:6">Online Programming Contests:</b> Here are some of the websites that I really love to compete at: <br>
+	<a href="https://www.topcoder.com" target="_blank"><img src="img/topcoder.png" alt="topcoder" style="width:213px; height:31px; margin: 5px;"> </a>
+	<a href="http://codeforces.com/" target="_blank"><img src="img/codeforces.png" alt="codeforces" style="width:264px; height:31px; margin: 5px;"></a>
+	<a href="https://leetcode.com/" target="_blank"><img src="img/leetcode.png" alt="leetcode" style="width:144px; height:31px; margin: 5px;"></a>
+
 	</div>
 </row> <!-- row ends -->
